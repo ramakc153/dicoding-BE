ea4641969345cf5ce628020997aaac21a81bcbcc
rama wandya <ramawandia153@gmail.com>
2023-07-18 22:57:55 +0700

    add dollar curly bracket param on return

--- routes.js
+++ routes.js
@@ -3,35 +3,35 @@ const routes = [
         method : 'GET',
         path : '/',
         handler : (request, h) =>{
-            return `this is homepage sir`
+            return `this is ${request.path} page using ${request.method} sir`
         }
     },
     {
         method : '*',
         path : '/',
         handler : (request, h) =>{
-            return `there is no other method in homepage sir`
+            return `there is no ${request.method} in ${request.path} page sir`
         }
     },
     {
         method : 'GET',
         path : '/about',
         handler : (request, h) =>{
-            return `this is about page sir`
+            return `this is ${request.path} page sir`
         }
     },
     {
         method : '*',
         path : '/about',
         handler : (request, h) =>{
-            return `there is no other method in about page sir`
+            return `there is no ${request.method} in ${request.path} page sir`
         }
     },
     {
         method : '*',
         path : '/{any*}',
         handler : (request, h) =>{
-            return `there is no such  page sir`
+            return `there is no such ${request.path} page sir`
         }
     },
 ]
